f134d791b0e7b62520f19903a585816bed62ee96
Martin Prajapati <113797260+martin-3107@users.noreply.github.com>
2022-10-27 12:13:11 +0530

    Update 10.odd_even.py

--- Practice Problem/10.odd_even.py
+++ Practice Problem/10.odd_even.py
@@ -1,7 +1,7 @@
 #Find odd or even number using python
-number = int(input("Enter any number :- "))
+num = int(input("Enter a number :- "))
 
-if number % 2 == 0:
-	print("Your input number is even")
+if num % 2 == 0:
+	print("Input number is even")
 else:
-	print("Your iput number is odd")	
+	print("Input number is odd")	
